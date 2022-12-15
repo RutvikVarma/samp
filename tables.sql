88417960a6b50177fecd409b2b8c2a1d5c6aaf66
Rutvik Krishna Varma Chekuri <95970475+RutvikVarma@users.noreply.github.com>
2022-12-15 12:11:00 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,16 +1,16 @@
 create table lotak_bank(
 account_no number(20) primary key,
 name varchar2(100),
-balance number(100,5));
+balance number(38,5));
 
 create table lotak_gpey(
 txn_no number(10) primary key,
-amt number(100,2));
+amt number(38,2));
 
 create table lotak_phonpe(
 txn_no number(10) primary key,
-amt number(100,2));
+amt number(38,2));
 
 create table lotak_petym(
 txn_no number(10) primary key,
-amt number(100,2));
+amt number(38,2));
